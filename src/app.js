ecab0ae38615b0983c9dc2ce6f4000298014bb25
vieerr <olivier_paspuel@hotmail.com>
2025-02-12 19:49:46 -0500

    feat: dog minimal routes added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
+const mongoose = require('mongoose');
 const express = require("express");
 
 const app = express();
 const port = 3000;
 
+
+console.log('connecting to', MONGODB_URI);
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error.message);
+  });
+
+
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Health check");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
