de5c8acf55120c3432b8c95dfa6f26a4a82412e1
vieerr <olivier_paspuel@hotmail.com>
2025-02-18 09:24:25 -0500

    feat: user routes added

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ const express = require("express");
 
 const dogRoutes = require("./modules/dogs/dog.routes");
 
+const userRoutes = require("./modules/users/user.routes");
+
 const router = express.Router();
 
 router.use("/dogs", dogRoutes);
+
+router.use("/users", userRoutes);
 
 router.get("/health", (req, res) => {
   res.send("Welcome to the Adogtame API");
